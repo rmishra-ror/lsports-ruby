602731066a99c3ee1d4c52bcab02a87bb8d75f2b
Varun Sharma <vsharma@gammastack.com>
2020-04-15 20:30:10 +0530

    Add prematch & reference APIs

--- lib/lsports/api/reference.rb
+++ lib/lsports/api/reference.rb
@@ -2,8 +2,59 @@
 
 module Api
   module Reference
-    def sports
-      get("GetSports")
+    # SPORTS
+    # Get list of all the sports
+    # GET /GetSports{?lang, indent}
+    # Parameters
+    # Name: Description
+    # lang: Response content language. Default value: English
+    # indent: Response content will be indented
+    def sports(params = {})
+      get("GetSports", params: params)
+    end
+
+    # LOCATIONS
+    # Get list of all the locations
+    # GET /GetLocations{?lang, indent}
+    # Parameters
+    # Name: Description
+    # lang: Response content language. Default value: English
+    # indent: Response content will be indented
+    def locations(params = {})
+      get("GetLocations", params: params)
+    end
+
+    # LEAGUES
+    # Get list of all the leagues
+    # GET /GetLeagues{?lang, indent}
+    # Parameters
+    # Name: Description
+    # lang: Response content language. Default value: English
+    # indent: Response content will be indented
+    def leagues(params = {})
+      get("GetLeagues", params: params)
+    end
+
+    # BOOKMAKERS
+    # Get list of all the bookmakers
+    # GET /GetBookmakers{?lang, indent}
+    # Parameters
+    # Name: Description
+    # lang: Response content language. Default value: English
+    # indent: Response content will be indented
+    def bookmakers(params = {})
+      get("GetBookmakers", params: params)
+    end
+    
+    # MARKETS
+    # Get list of all the markets
+    # GET /GetMarkets{?lang, indent}
+    # Parameters
+    # Name: Description
+    # lang: Response content language. Default value: English
+    # indent: Response content will be indented
+    def markets(params = {})
+      get("GetMarkets", params: params)
     end
   end
 end
